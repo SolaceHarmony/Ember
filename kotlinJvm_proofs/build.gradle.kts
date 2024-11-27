a51ce57d880ad4c32c799a5c7dd1db71dfeb0e33
sydneyrenee <sydney@solace.ofharmony.ai>
2024-11-26 16:48:02 -0800

    Initial test of IdeaJ's WSL support... not good.

--- kotlinJvm_proofs/build.gradle.kts
+++ kotlinJvm_proofs/build.gradle.kts
@@ -1,8 +1,7 @@
 // build.gradle.kts
 
 plugins {
-    kotlin("jvm") version "2.0.21"
-    kotlin("plugin.serialization") version "2.0.21"
+    kotlin("jvm") version "2.0.20"
 }
 
 group = "ai.solace.core.kognitive.kotlinJvm_proofs"
@@ -11,47 +10,27 @@ version = "0.0.1"
 
 repositories {
     mavenCentral()
-    maven {
-        name = "reposiliteRepository"
-        url = uri("https://maven.sciprog.center/kscience")
-    }
 }
 
 dependencies {
     implementation("org.jetbrains.kotlinx:kandy-lets-plot:0.7.1")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.9.0")
-    runtimeOnly("space.kscience:kmath-core:0.3.1")
-    implementation("space.kscience:plotlykt-script:0.6.0")
-
-
-    // Scripting engine
-    implementation("org.jetbrains.kotlin:kotlin-scripting-jsr223:2.0.0")
-
-    implementation("io.ktor:ktor-server-call-logging-jvm:2.3.12")
-    implementation("ch.qos.logback:logback-classic:1.5.11")
-    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:2.3.12")
-
-    // Swagger/OpenAPI Support
-    implementation("io.ktor:ktor-server-openapi-jvm:2.3.12")
-    implementation("io.ktor:ktor-server-swagger-jvm:2.3.12")
-
-    // WebSockets and Sessions
-    implementation("io.ktor:ktor-server-sessions-jvm:2.3.12")
-    implementation("io.ktor:ktor-server-websockets-jvm:2.3.12")
-
-    // Observability
-    implementation("io.ktor:ktor-server-metrics-micrometer-jvm:2.3.12")
-    implementation("io.micrometer:micrometer-registry-prometheus:1.13.2")
-
-    // Authentication
-    implementation("io.ktor:ktor-server-auth-jvm:2.3.12")
-
+    implementation("space.kscience:kmath-core-jvm:0.3.1")
+    implementation("org.jetbrains.kotlinx:multik-core:0.2.3")
+    implementation("org.jetbrains.kotlinx:multik-default:0.2.3")
+    implementation("org.jetbrains.kotlinx:multik-openblas:0.2.3")
+    implementation("org.jetbrains.kotlinx:kotlin-deeplearning-tensorflow:0.5.2")
+    implementation("org.jetbrains.kotlinx:kotlin-deeplearning-api:0.3.0")
+    implementation("org.jetbrains.kotlinx:kotlin-deeplearning-impl-jvm:0.5.1")
+    implementation("org.jetbrains.kotlinx:kotlin-deeplearning-onnx-jvm:0.5.1")
+    implementation("org.jetbrains.kotlinx:kotlin-deeplearning-visualization-jvm:0.5.1")
     // Testing
-    testImplementation("io.ktor:ktor-server-tests-jvm:2.3.12")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.0.21")
 }
 
+
 kotlin {
+    jvmToolchain(21)
     compilerOptions {
         apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
     }
